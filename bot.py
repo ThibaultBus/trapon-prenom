de1fd8f9752b1d9ab4e3b650ef7670766433e4b4
Thibault Bustarret <thibault.bustarret@etu.univ-nantes.fr>
2021-11-06 21:07:52 +0100

    Modified dotenv usage for compatibility with Dash-based deployment

--- bot.py
+++ bot.py
@@ -2,13 +2,14 @@ import discord
 from trapon_client import TraponClient
 from loop import Loop
 from name_generator import NameGenerator
-from dotenv import dotenv_values
+from dotenv import load_dotenv
+import os
 
-config = dotenv_values(".env")
+config = load_dotenv()
 
-client = TraponClient(config["TARGET_USER"])
-name_generator = NameGenerator(config["FILE"])
-loop = Loop(int(config["LAUNCH_HOUR"]), client, name_generator)
+client = TraponClient(os.getenv("TARGET_USER"))
+name_generator = NameGenerator(os.getenv("FILE"))
+loop = Loop(int(os.getenv("LAUNCH_HOUR")), client, name_generator)
 
 
 @ client.event
@@ -26,4 +27,4 @@ async def on_ready():
     await client.on_login()
 
 
-client.run(config["TOKEN"])
+client.run(os.getenv("TOKEN"))
